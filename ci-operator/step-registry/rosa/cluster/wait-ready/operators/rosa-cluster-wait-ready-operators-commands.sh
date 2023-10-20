e7164d304da530d45c1de4101ec6eb94dcd0c060
Pawel Paszki <ppaszki@redhat.com>
2023-10-20 12:01:04 +0100

    test: fm ci

--- ci-operator/step-registry/rosa/cluster/wait-ready/operators/rosa-cluster-wait-ready-operators-commands.sh
+++ ci-operator/step-registry/rosa/cluster/wait-ready/operators/rosa-cluster-wait-ready-operators-commands.sh
@@ -25,6 +25,8 @@ CO_STATUS_LOG="${ARTIFACT_DIR}/co_status.log"
 oc wait clusteroperators --all --for=condition=Progressing=false --timeout=60m > "${CO_STATUS_LOG}" 2>&1 || true
 cat "${CO_STATUS_LOG}"
 
+sleep 5
+
 ## If waiting operators timeout, call ocm-qe to analyze the root cause.
 costatus=$(cat "${CO_STATUS_LOG}")
 if [[ "${costatus}" =~ "timed out" ]]; then
